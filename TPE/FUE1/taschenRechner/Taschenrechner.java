953c441c1f6e9b0cbcaf3cc64ce3af56cb892e0c
Max Jando <max.jando@googlemail.com>
2013-09-29 09:52:05 +0200

    FUE1 - taschenRechner

---
+++ TPE/FUE1/taschenRechner/Taschenrechner.java
@@ -0,0 +1,39 @@
+package taschenRechner;
+/**
+ * @author: Max Jando
+ * Date: 29.09.13
+ * Time: 09:50
+ * Diese Klasse symbolisiert einen Taschenrechner. Gültige Operatoren sind +,-,*,/
+ */
+public class Taschenrechner {
+
+    public static void main(String[] args) {
+        Taschenrechner t = new Taschenrechner();
+        System.out.println(t.rechne(3, 5, '+'));
+        System.out.println(t.rechne(8, 6, '-'));
+        System.out.println(t.rechne(3, 3, '*'));
+        System.out.println(t.rechne(20, 10, '/'));
+    }
+
+    private double rechne(double ersterSummand, double zweiterSummand, char operator) {
+        double ergebnis;
+        switch (operator) {
+            case '+':
+                ergebnis = ersterSummand + zweiterSummand;
+                break;
+            case '-':
+                ergebnis = ersterSummand - zweiterSummand;
+                break;
+            case '*':
+                ergebnis = ersterSummand * zweiterSummand;
+                break;
+            case '/':
+                ergebnis = ersterSummand / zweiterSummand;
+                break;
+            default:
+                ergebnis = Double.NaN;
+        }
+        return ergebnis;
+    }
+
+}
