4f3aa28214613597d9537652a498604bdda33293
Max Jando <max.jando@googlemail.com>
2013-10-11 11:33:37 +0200

    Addieren

--- TPE/uebungen/src/tpe/fruh_razzaq_jando/pue1/Bruch.java
+++ TPE/uebungen/src/tpe/fruh_razzaq_jando/pue1/Bruch.java
@@ -4,65 +4,96 @@ import java.math.*;
 
 /**
  * Diese Klasse implementiert einen Bruch
- * 
+ *
  * @author TPE_UIB_01
  */
 public class Bruch {
 
-	// ================================================================================
-	// Properties
-	// ================================================================================
+    // ================================================================================
+    // Properties
+    // ================================================================================
 
-	private long nenner, zaehler, ganze;
+    private long nenner, zaehler, ganze;
 
-	// ================================================================================
-	// Constructors
-	// ================================================================================
+    // ================================================================================
+    // Constructors
+    // ================================================================================
 
-	Bruch(long zaehler, long nenner) {
-		this.zaehler = zaehler;
-		this.nenner = nenner;
-		kuerze();
-	}
+    Bruch(long zaehler, long nenner) {
+        this.zaehler = zaehler;
+        this.nenner = nenner;
+        kuerze();
+    }
 
-	Bruch(long zaehler, long nenner, long ganze) {
-		this.zaehler = zaehler;
-		this.nenner = nenner;
-		this.ganze = ganze;
-		kuerze();
-	}
+    Bruch(long ganze, long zaehler, long nenner) {
+        this.zaehler = zaehler;
+        this.nenner = nenner;
+        this.ganze = ganze;
+        kuerze();
+    }
 
-	// ================================================================================
-	// Accessors
-	// ================================================================================
+    // ================================================================================
+    // Accessors
+    // ================================================================================
 
-	public long getNenner() {
-		return nenner;
-	}
+    public long getNenner() {
+        return nenner;
+    }
 
-	public void setNenner(long nenner) {
-		this.nenner = nenner;
-	}
+    public void setNenner(long nenner) {
+        this.nenner = nenner;
+    }
 
-	public long getZaehler() {
-		return zaehler;
-	}
+    public long getZaehler() {
+        return zaehler;
+    }
 
-	public void setZaehler(long zaehler) {
-		this.zaehler = zaehler;
-	}
+    public void setZaehler(long zaehler) {
+        this.zaehler = zaehler;
+    }
 
-	public long getGanze() {
-		return ganze;
-	}
+    public long getGanze() {
+        return ganze;
+    }
 
-	public void setGanze(long ganze) {
-		this.ganze = ganze;
-	}
+    public void setGanze(long ganze) {
+        this.ganze = ganze;
+    }
 
-	// ================================================================================
-	// Methods
-	// ================================================================================
+    // ================================================================================
+    // Methods
+    // ================================================================================
+
+    public Bruch addiere(Bruch zweiterBruch) {
+        boolean echt = false;
+        Bruch tmpBruch1;
+        Bruch tmpBruch2;
+        if (this.ganze != 0) {
+            echt = true;
+            tmpBruch1 = new Bruch(this.ganze, this.zaehler, this.nenner);
+        } else {
+            tmpBruch1 = new Bruch(this.zaehler, this.nenner);
+        }
+        if (zweiterBruch.ganze != 0) {
+            echt = true;
+            tmpBruch2 = new Bruch(zweiterBruch.ganze, zweiterBruch.zaehler, zweiterBruch.nenner);
+        } else {
+            tmpBruch2 = new Bruch(zweiterBruch.zaehler, zweiterBruch.nenner);
+        }
+        tmpBruch1.unechterBruch();
+        tmpBruch2.unechterBruch();
+        System.out.println(tmpBruch1);
+        System.out.println(tmpBruch2);
+        long zaehler = (tmpBruch1.zaehler * tmpBruch2.nenner) + (tmpBruch2.zaehler * tmpBruch1.nenner);
+        System.out.println(zaehler);
+        Bruch ergebnisBruch = new Bruch(zaehler, tmpBruch1.nenner * tmpBruch2.nenner);
+        ergebnisBruch.kuerze();
+        if (echt) {
+            ergebnisBruch.echterBruch();
+        }
+        return ergebnisBruch;
+    }
+
 
     public void unechterBruch() {
         if (this.ganze != 0) {
@@ -78,28 +109,28 @@ public class Bruch {
         }
     }
 
-	private void kuerze() {
-		long ggt = getGGT(Math.min(zaehler, nenner));
+    private void kuerze() {
+        long ggt = getGGT(Math.min(zaehler, nenner));
 
-		this.zaehler = this.zaehler / ggt;
-		this.nenner = this.nenner / ggt;
-	}
+        this.zaehler = this.zaehler / ggt;
+        this.nenner = this.nenner / ggt;
+    }
 
-	private long getGGT(long aktuelleZahl) {
-		if (zaehler % aktuelleZahl == 0 && nenner % aktuelleZahl == 0)
-			return aktuelleZahl;
-		else
-			return getGGT(aktuelleZahl - 1);
-	}
+    private long getGGT(long aktuelleZahl) {
+        if (zaehler % aktuelleZahl == 0 && nenner % aktuelleZahl == 0)
+            return aktuelleZahl;
+        else
+            return getGGT(aktuelleZahl - 1);
+    }
 
-	@Override
-	public String toString() {
-		if (ganze == 0)
-			return zaehler + "/" + nenner;
-		else if (zaehler == 0 && nenner == 0)
-			return ganze + "";
-		else
-			return ganze + " " + zaehler + "/" + nenner;
-	}
+    @Override
+    public String toString() {
+        if (ganze == 0)
+            return zaehler + "/" + nenner;
+        else if (zaehler == 0 && nenner == 0)
+            return ganze + "";
+        else
+            return ganze + " " + zaehler + "/" + nenner;
+    }
 
 }
