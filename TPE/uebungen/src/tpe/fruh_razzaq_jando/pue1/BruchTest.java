a818b55c372964c30782ddfe017ca9a323e66cb1
Kaeltis <info@kaeltis.de>
2013-10-11 11:11:56 +0200

    Update BruchTest.java

--- TPE/uebungen/src/tpe/fruh_razzaq_jando/pue1/BruchTest.java
+++ TPE/uebungen/src/tpe/fruh_razzaq_jando/pue1/BruchTest.java
@@ -6,20 +6,46 @@ import org.junit.Test;
 
 public class BruchTest {
 
-    @Test
-    public void testBruchLongLong() {
-        Bruch bruch1 = new Bruch(1, 2);
-        Bruch bruch2 = new Bruch(45, 20, 3);
-        System.out.println(bruch2);
-        bruch2.unechterBruch();
-        System.out.println("Unechter Bruch: " + bruch2);
-        bruch2.echterBruch();
-        System.out.println("Echter Bruch: " + bruch2);
-    }
+	@Test
+	public void testGetDezimalzahl() {
+		Bruch bruch1 = new Bruch(19, 2);
+		assertEquals(9.5D, bruch1.getDezimalzahl(), 0.1D);
 
-	/*@Test
-    public void testBruchLongLongLong() {
-		fail("Not yet implemented");
+		Bruch bruch2 = new Bruch(16, 2);
+		assertEquals(8.0, bruch2.getDezimalzahl(), 0.1);
+
+		Bruch bruch3 = new Bruch(5, 10);
+		assertEquals(0.5, bruch3.getDezimalzahl(), 0.1);
 	}
-           */
+
+	@Test
+	public void testEchterBruch() {
+		Bruch bruch1e = new Bruch(4, 4, 5);
+		Bruch bruch1 = new Bruch(24, 5);
+		bruch1.echterBruch();
+
+		assertEquals(bruch1e.toString(), bruch1.toString());
+
+		Bruch bruch2e = new Bruch(9, 2, 5);
+		Bruch bruch2 = new Bruch(47, 5);
+		bruch2.echterBruch();
+
+		assertEquals(bruch2e.toString(), bruch2.toString());
+	}
+
+	@Test
+	public void testUnechterBruch() {
+		Bruch bruch1 = new Bruch(4, 4, 5);
+		Bruch bruch1e = new Bruch(24, 5);
+		bruch1.unechterBruch();
+
+		assertEquals(bruch1e.toString(), bruch1.toString());
+
+		Bruch bruch2 = new Bruch(9, 2, 5);
+		Bruch bruch2e = new Bruch(47, 5);
+		bruch2.unechterBruch();
+
+		assertEquals(bruch2e.toString(), bruch2.toString());
+	}
+
 }
