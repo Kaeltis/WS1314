5dea8b42289ac044b58e2abc0fc41ed8e19bc9fb
Max Jando <max.jando@googlemail.com>
2013-10-11 09:27:48 +0200

    Getter und Setter

--- TPE/fruh_razzaq_jando/Bruch.java
+++ TPE/fruh_razzaq_jando/Bruch.java
@@ -1,3 +1,32 @@
-
+/**
+ * @author TPE_UIB_01
+ * Diese Klasse implementiert einen Bruch
+ */
 public class Bruch {
+
+    private long nenner, zähler, ganzzahlerAnteil;
+
+    public long getNenner() {
+        return nenner;
+    }
+
+    public void setNenner(long nenner) {
+        this.nenner = nenner;
+    }
+
+    public long getZähler() {
+        return zähler;
+    }
+
+    public void setZähler(long zähler) {
+        this.zähler = zähler;
+    }
+
+    public long getGanzzahlerAnteil() {
+        return ganzzahlerAnteil;
+    }
+
+    public void setGanzzahlerAnteil(long ganzzahlerAnteil) {
+        this.ganzzahlerAnteil = ganzzahlerAnteil;
+    }
 }
