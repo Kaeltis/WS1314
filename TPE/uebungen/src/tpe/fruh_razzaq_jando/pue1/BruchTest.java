34d43d563ca42aaae7d2787d6727f10eea590924
Kaeltis <info@kaeltis.de>
2013-10-11 11:35:01 +0200

    Update BruchTest.java

--- TPE/uebungen/src/tpe/fruh_razzaq_jando/pue1/BruchTest.java
+++ TPE/uebungen/src/tpe/fruh_razzaq_jando/pue1/BruchTest.java
@@ -48,4 +48,120 @@ public class BruchTest {
 		assertEquals(bruch2e.toString(), bruch2.toString());
 	}
 
+	@Test
+	public void testAddiere() {
+		Bruch bruch1a = new Bruch(1, 6);
+		Bruch bruch1b = new Bruch(2, 15);
+		Bruch bruch1exp = new Bruch(3, 10);
+		Bruch bruch1act = bruch1a.addiere(bruch1b);
+
+		assertEquals(bruch1exp.toString(), bruch1act.toString());
+
+		Bruch bruch2a = new Bruch(1, 5);
+		Bruch bruch2b = new Bruch(1, 5);
+		Bruch bruch2exp = new Bruch(2, 5);
+		Bruch bruch2act = bruch2a.addiere(bruch2b);
+
+		assertEquals(bruch2exp.toString(), bruch2act.toString());
+
+		Bruch bruch3a = new Bruch(2, 10);
+		Bruch bruch3b = new Bruch(3, 5);
+		Bruch bruch3exp = new Bruch(8, 10);
+		Bruch bruch3act = bruch3a.addiere(bruch3b);
+
+		assertEquals(bruch3exp.toString(), bruch3act.toString());
+	}
+
+	@Test
+	public void testSubtrahiere() {
+		Bruch bruch1a = new Bruch(3, 10);
+		Bruch bruch1b = new Bruch(1, 6);
+		Bruch bruch1exp = new Bruch(2, 15);
+		Bruch bruch1act = bruch1a.subtrahiere(bruch1b);
+
+		assertEquals(bruch1exp.toString(), bruch1act.toString());
+
+		Bruch bruch2a = new Bruch(2, 5);
+		Bruch bruch2b = new Bruch(1, 5);
+		Bruch bruch2exp = new Bruch(1, 5);
+		Bruch bruch2act = bruch2a.subtrahiere(bruch2b);
+
+		assertEquals(bruch2exp.toString(), bruch2act.toString());
+
+		Bruch bruch3a = new Bruch(8, 10);
+		Bruch bruch3b = new Bruch(2, 10);
+		Bruch bruch3exp = new Bruch(3, 5);
+		Bruch bruch3act = bruch3a.subtrahiere(bruch3b);
+
+		assertEquals(bruch3exp.toString(), bruch3act.toString());
+	}
+
+	@Test
+	public void testSubtrahiere() {
+		Bruch bruch1a = new Bruch(3, 10);
+		Bruch bruch1b = new Bruch(1, 6);
+		Bruch bruch1exp = new Bruch(2, 15);
+		Bruch bruch1act = bruch1a.subtrahiere(bruch1b);
+
+		assertEquals(bruch1exp.toString(), bruch1act.toString());
+
+		Bruch bruch2a = new Bruch(2, 5);
+		Bruch bruch2b = new Bruch(1, 5);
+		Bruch bruch2exp = new Bruch(1, 5);
+		Bruch bruch2act = bruch2a.subtrahiere(bruch2b);
+
+		assertEquals(bruch2exp.toString(), bruch2act.toString());
+
+		Bruch bruch3a = new Bruch(8, 10);
+		Bruch bruch3b = new Bruch(2, 10);
+		Bruch bruch3exp = new Bruch(3, 5);
+		Bruch bruch3act = bruch3a.subtrahiere(bruch3b);
+
+		assertEquals(bruch3exp.toString(), bruch3act.toString());
+	}
+
+	@Test
+	public void testMultipliziere() {
+		Bruch bruch1a = new Bruch(3, 10);
+		Bruch bruch1b = new Bruch(1, 6);
+		Bruch bruch1exp = new Bruch(3, 60);
+		Bruch bruch1act = bruch1a.multipliziere(bruch1b);
+
+		assertEquals(bruch1exp.toString(), bruch1act.toString());
+
+		Bruch bruch2a = new Bruch(3, 6);
+		int zahl2b = 5;
+		Bruch bruch2exp = new Bruch(15, 6);
+		Bruch bruch2act = bruch2a.multipliziere(zahl2b);
+
+		assertEquals(bruch2exp.toString(), bruch2act.toString());
+	}
+
+	@Test
+	public void testDividiere() {
+		Bruch bruch1a = new Bruch(3, 60);
+		Bruch bruch1b = new Bruch(1, 6);
+		Bruch bruch1exp = new Bruch(3, 10);
+		Bruch bruch1act = bruch1a.dividiere(bruch1b);
+
+		assertEquals(bruch1exp.toString(), bruch1act.toString());
+
+		Bruch bruch2a = new Bruch(3, 60);
+		int zahl2b = 10;
+		Bruch bruch2exp = new Bruch(3, 6);
+		Bruch bruch2act = bruch2a.dividiere(zahl2b);
+
+		assertEquals(bruch2exp.toString(), bruch2act.toString());
+	}
+
+	@Test
+	public void testPotenziere() {
+		Bruch bruch1a = new Bruch(2, 3);
+		int zahl1b = 0;
+		Bruch bruch1exp = new Bruch(2, 15);
+		Bruch bruch1act = bruch1a.potenziere(zahl1b);
+
+		assertEquals(bruch1exp.toString(), bruch1act.toString());
+	}
+
 }
